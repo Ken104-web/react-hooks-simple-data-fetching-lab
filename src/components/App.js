75531f7cc7867989511ff4da45e7acf399523056
KenCorp <kennethwarui35@gmail.com>
2023-07-26 21:15:16 +0300

    test done

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,35 @@
 // create your App component here
+import React, { useEffect, useState } from "react";
+
+
+function App() {
+    const [DogImage, setDogImage] = useState('')
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then((resp) => resp.json())
+        .then((data) => {
+        setDogImage(data.message)
+    })
+    .catch((error) => {
+        console.error('Error');
+    });
+    }, []);
+    return(
+        <div>
+        <h1>Random Dog</h1>
+        {DogImage ? (
+        <img 
+        alt="A Random Dog"
+        src="https://images.dog.ceo/breeds/bulldog-english/mami.jpg"
+        /> ) : (
+            <p>Loading...</p>
+        )
+        }
+
+        </div>
+    );
+
+
+}
+export default App;
